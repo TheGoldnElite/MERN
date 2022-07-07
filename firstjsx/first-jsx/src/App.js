1f7bc700d48f51e751f141d33b56260d11209eb7
TheGoldnElite <jsherbstritt@yahoo.com>
2022-07-07 12:24:51 -0400

    commit on main

---
+++ firstjsx/first-jsx/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello Dojo!</h1>
+        <ul>Things I need to do:
+          <li>Learn React</li>
+          <li>Climb Mt.Everest</li>
+          <li>Run a Marathon</li>
+          <li>Feed The dogs</li>
+
+        </ul>
+      
+    </div>
+  );
+}
+
+export default App;
